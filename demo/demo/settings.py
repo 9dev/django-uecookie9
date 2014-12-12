e0fee88f8b28948bf9f5c9a35ec9baecc47ee554
9dev <9devmail@gmail.com>
2014-12-12 16:43:22 +0100

    Fixed settings in demo

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -87,3 +87,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+###
+
+from django.conf import settings
+TEMPLATE_CONTEXT_PROCESSORS = settings.TEMPLATE_CONTEXT_PROCESSORS + (
+    'django.core.context_processors.request',
+)
+
+
